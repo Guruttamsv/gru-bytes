402642d92ac98ff8b8d8055d0dcd6c9ea8614798
Guruttamsv <Guruttamsv511@gmail.com>
2024-08-27 12:44:33 +0100

    add: Fixing Component Position 2-correction

--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef} from "react";
 import WorkGrid from "../components/WorkPage/WorkGrid";
 import "../pagesCSS/WorkPage.css";
 
